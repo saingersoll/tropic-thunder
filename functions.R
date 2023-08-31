e273027827f8182ddb501cb117c5a066e45f61db
saingersoll <sofia.ingersoll@Outlook.com>
2023-08-31 22:34:36 +0000

    cleaning targets

--- functions.R
+++ functions.R
@@ -10,5 +10,22 @@ scrub_funct <- function(site_df) {
   site_df %>% 
   clean_names() %>% 
     mutate(clean_date = lubridate::ymd(sample_date)) %>% 
-    select(clean_date, no3_n, k, sample_id)
+    mutate(year = lubridate::year(clean_date)) %>% 
+    select(clean_date, no3_n, k, sample_id) %>% 
+    filter(year %in% seq[1989:1994])
 } 
+
+
+# Binding Rows of Data Sets
+binding_data <- function(site_df, site_df2) {
+  site_df %>% 
+  bind_rows(site_df2)
+}
+
+# Ggplot Graphing Concentration 
+k_plot <- ggplot(data = total_data , aes(x = clean_date, y = k))+
+  geom_line(aes(color = sample_id)) +
+  labs(x = "Year",
+       y = "K mg i-1")
+
+k_plot
